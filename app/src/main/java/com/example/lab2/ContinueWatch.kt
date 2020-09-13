9a21fa6cbd67fc8fe7ca6d8502c1a8fbf451d771
dVede <dvede123@yandex.ru>
2020-09-13 18:40:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab2/ContinueWatch.kt
@@ -0,0 +1,46 @@
+package com.example.lab2
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ContinueWatch : AppCompatActivity() {
+    
+    private var secondsElapsed: Int = 0
+    private var isRunning: Boolean = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_continue_watch)
+        if (savedInstanceState != null) {
+            secondsElapsed = savedInstanceState.getInt("seconds")
+        }
+        findViewById<TextView>(R.id.textSecondsElapsed).text =
+                String.format(getString(R.string.seconds_elapsed) + " %s", secondsElapsed++)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isRunning = true
+        val backgroundThread = Thread {
+            while (isRunning) {
+                Thread.sleep(1000)
+                findViewById<TextView>(R.id.textSecondsElapsed).post {
+                    findViewById<TextView>(R.id.textSecondsElapsed).text =
+                            String.format(getString(R.string.seconds_elapsed) + " %s", secondsElapsed++)
+                }
+            }
+        }
+        backgroundThread.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isRunning = false
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("seconds", secondsElapsed)
+    }
+}
---
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.lab2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (BuildConfig.LOG) Log.d(BuildConfig.LOG_TAG, getClass().getSimpleName() + ".onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (BuildConfig.LOG) Log.d(BuildConfig.LOG_TAG, getClass().getSimpleName() + ".onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (BuildConfig.LOG) Log.d(BuildConfig.LOG_TAG, getClass().getSimpleName() + ".onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (BuildConfig.LOG) Log.d(BuildConfig.LOG_TAG, getClass().getSimpleName() + ".onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (BuildConfig.LOG) Log.d(BuildConfig.LOG_TAG, getClass().getSimpleName() + ".onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (BuildConfig.LOG) Log.d(BuildConfig.LOG_TAG, getClass().getSimpleName() + ".onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (BuildConfig.LOG) Log.d(BuildConfig.LOG_TAG, getClass().getSimpleName() + ".onDestroy");
+    }
+
+    public void onButtonClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.button:
+                Intent intent1 = new Intent(this, Floating.class);
+                startActivity(intent1);
+                break;
+            case R.id.button2:
+                Intent intent2 = new Intent(this, ContinueWatch.class);
+                startActivity(intent2);
+                break;
+        }
+    }
+}
